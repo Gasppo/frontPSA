94adf82c41edec9f11e6fda079ae4e8ef8b01932
Gaspar <Gaspar.GarciaBarnetche@axxiome.com>
2022-06-20 19:53:32 -0300

    useLocation

--- src/pages/recursos/cargaDeHoras.tsx
+++ src/pages/recursos/cargaDeHoras.tsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 import { MultiSelect } from "react-multi-select-component";
-import {  TextField } from '@mui/material';
+import { TextField } from '@mui/material';
 import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { proyectsAPI } from "../../components/dev/URIs"
 import { SelectProyect, Proyect, Task } from '../../components/types/resourcesTypes'
 import TasksTableRow from '../../components/UI/Horas/TasksTableRow'
@@ -18,77 +18,83 @@ interface CargaDeHorasProps {
 
 
 
-const CargaDeHoras= (props: CargaDeHorasProps,) => {
+const CargaDeHoras = (props: CargaDeHorasProps,) => {
 
     const [tasks, setTasks] = useState<Task[]>([]);
     const [showAddModal, setShowAddModal] = useState(false)
 
+    const { state }: any = useLocation()
+
     useEffect(() => {
         fetchTasks()
-    },[]);
+    }, []);
 
-const handleAddOpen = () => {
-    setShowAddModal(true)
-}
-const handleSubmit = () =>{
-    /* cargar las horas */
-    alert("Las horas han sido cargadas")
-}
+    const handleAddOpen = () => {
+        setShowAddModal(true)
+    }
+    const handleSubmit = () => {
+        /* cargar las horas */
+        alert("Las horas han sido cargadas")
+    }
 
-const handleClose = () => {
-    setShowAddModal(false);
-}
+    const handleClose = () => {
+        setShowAddModal(false);
+    }
 
 
-    const fetchTasks = () => { 
+    const fetchTasks = () => {
         fetch('https://modulo-proyectos-psa-2022.herokuapp.com/projects')
-        .then(res => res.json())
-        .then(res => {
-            console.log(res)
-            setTasks([])
-            let tasks:Task[] = []
-            res.forEach((element:Proyect) => {
-                element.tasks.forEach((item:Task) => {
-                    let proyectTask = {
-                        ...item,
-                        proyectName: element.name,
-                        proyectCode: element.code,
-                    }
-                    tasks.push(proyectTask)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                setTasks([])
+                let tasks: Task[] = []
+                res.forEach((element: Proyect) => {
+                    element.tasks.forEach((item: Task) => {
+                        let proyectTask = {
+                            ...item,
+                            proyectName: element.name,
+                            proyectCode: element.code,
+                        }
+                        tasks.push(proyectTask)
+                    });
+                    setTasks(tasks)
                 });
-            setTasks(tasks)
-            });
-            
-            
-        })
-        .catch(err => {
-            console.log(JSON.stringify(err))
-        })
+
+
+            })
+            .catch(err => {
+                console.log(JSON.stringify(err))
+            })
 
     }
 
-      
-return(    
-    <>
-   
-        
-        <div className="flex flex-row" >
-            <Link to={'/recursos/horasSemanales/carga/'} >
-                <Button>Volver atras</Button>
-            </Link>
+    useEffect(() => {
+        console.log(state?.items)
+    }, [state]);
+
+
+    return (
+        <>
+
+
+            <div className="flex flex-row" >
+                <Link to={'/recursos/horasSemanales/carga/'} >
+                    <Button>Volver atras</Button>
+                </Link>
             </div>
 
-        <div>
+            <div>
 
 
- 
-            <TextField disabled type='date'inputProps={{max: new Date().toISOString().slice(0, 10)}} defaultValue='2022-06-16'></TextField>
 
-            <TableContainer component={Paper} className="mt-10"  >
+                <TextField disabled type='date' inputProps={{ max: new Date().toISOString().slice(0, 10) }} defaultValue='2022-06-16'></TextField>
+
+                <TableContainer component={Paper} className="mt-10"  >
                     <Table>
                         <TableHead>
                             <TableRow>
-                            <TableCell align="left">Codigo de Proyecto</TableCell>
+                                <TableCell align="left">Codigo de Proyecto</TableCell>
                                 <TableCell align="left">Proyecto</TableCell>
                                 <TableCell align="left">Codigo de Tarea</TableCell>
                                 <TableCell align="left">Tarea</TableCell>
@@ -97,23 +103,23 @@ return(
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                        {tasks.map((row:Task)=><LoadHoursTableRow row={row} key={row._id}/>)}
+                            {state?.items.map((row: Task) => <LoadHoursTableRow row={row} key={row._id} />) || []}
                         </TableBody>
                     </Table>
-            </TableContainer>
-        </div>
+                </TableContainer>
+            </div>
 
-        <div className="self-end mr-10 border-2 text-center  rounded-xl shadow-lg text-slate-800 hover:bg-gray-200 hover:text-teal-600 transition-all duration-300 cursor-pointer">
-                    <Button onClick={handleAddOpen}>
+            <div className="self-end mr-10 border-2 text-center  rounded-xl shadow-lg text-slate-800 hover:bg-gray-200 hover:text-teal-600 transition-all duration-300 cursor-pointer">
+                <Button onClick={handleAddOpen}>
                     <div className="m-4"> Guardar</div>
-                    </Button>
+                </Button>
             </div>
             {/* <AddHourModal onSubmit={handleSubmit} onClose={handleClose} show={showAddModal}></AddHourModal> */}
-    
-        
+
+
         </>
-        
-    ) 
+
+    )
 }
 
 export default CargaDeHoras
