59518bc72ee2c808119abd6f47fac39156469d28
Ignacio Tornati <ignaciotornati@mbp-de-ignacio.lan>
2022-06-20 16:37:00 -0300

    Agrupacion de horas segun tarea

--- src/components/types/resourcesTypes.ts
+++ src/components/types/resourcesTypes.ts
@@ -1,7 +1,48 @@
+export type Task= {
+    _id: string;
+    priority:number;
+    name: string;
+    description: string;
+    effort:number;
+    resource:number;
+    code:number;
+    _v: number;
+    proyectCode: number;
+    proyectName: string;
+}
+
 export type Hours = {
     _id: string;
     hourAssignee:number;
     created: Date;
     _v: number;
+    task: Task;
+    duration: number;
 
 }
+
+export type SelectProyect = {
+    value: number,
+    label:string
+}
+
+export type Proyect = {
+    _id: string;
+    name: string;
+    creationDate: Date;
+    startDate: Date;
+    endDate: Date;
+    updateDate: Date;
+    type: string;
+    state: string;
+    client: number;
+    risk: number;
+    productId: number;
+    fase: number;
+    iteration:number;
+    description:string;
+    tasks:Task[];
+    code:number;
+}
+
+
