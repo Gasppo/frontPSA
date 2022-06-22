4f3e0e8fe0c104cf0090f808d6b14b3172f72c2c
Gaspar <Gaspar.GarciaBarnetche@axxiome.com>
2022-06-22 18:55:53 -0300

    fix: cosas

--- src/pages/recursos/cargaDeLicencias.tsx
+++ src/pages/recursos/cargaDeLicencias.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { MultiSelect } from "react-multi-select-component";
 import { TableFooter, TablePagination, TextField } from '@mui/material';
 import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
@@ -25,23 +25,23 @@ const CargaDeLicencias = (props: CargaDeLicenciasProps,) => {
     const [licenses, setLicenses] = useState<any>([])
     const [employees, setEmployees] = useState<any>([])
 
-    
+
     const handleAddOpen = () => {
         setShowAddModal(true)
     }
-  
+
     const handleEditOpen = (id: number) => {
         setCurrentID(id)
         setShowEditModal(true)
     }
-    
+
     const handleClose = () => {
         setShowAddModal(false)
         setShowEditModal(false)
     }
-  
+
     const handleSubmit = () => {
-       
+
         setShowAddModal(false)
         setShowEditModal(false)
     }
@@ -49,49 +49,47 @@ const CargaDeLicencias = (props: CargaDeLicenciasProps,) => {
     const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
         setPage(newPage);
     };
-  
+
     const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
     };
-    const  getLicenses = ()=>{
 
-        fetch('https://modulo-recursos-psa.herokuapp.com/employees')
+    const getEmployees = useCallback(async () => {
+       const empleados = await fetch('https://modulo-recursos-psa.herokuapp.com/employees').then(res => res.json())
+       setEmployees(empleados)
+    }, [])
+
+    const getLicenses = useCallback(() => {
+         fetch('https://modulo-recursos-psa.herokuapp.com/licenses')
             .then(res => res.json())
             .then(res => {
-                setEmployees(res)
-
-                fetch('https://modulo-recursos-psa.herokuapp.com/licenses')
-            .then(res => res.json())
-            .then(res => {
-                let licencias:any[] = []
-                res.forEach((item:any) =>{
-                    let empleado = employees.filter((itemE:any) => itemE.legajo === item.licensedPersonCode)
-                    let licencia = {
+                console.log(employees)
+                const licencias = res.map((item: License) => {
+                    const empleado = employees.find((el: any) => item.licensedPersonCode === el.legajo)
+                    return {
                         ...item,
-                        employeeName: empleado[0].Apellido + ', ' + empleado[0].Nombre
+                        employeeName: `${empleado?.Nombre || 'N/A'} ${empleado?.Apellido || 'N/A'}`,
                     }
-                    licencias.push(licencia)
                 })
-                console.log(licencias)
                 setLicenses(licencias)
-            }) 
-            .catch(err => {
-                console.log(JSON.stringify(err))
+                setLoading(false)
             })
-            }) 
             .catch(err => {
-                console.log(JSON.stringify(err))
+                console.log(err)
             })
-            setLoading(false)
 
-    }
+    }, [employees])
+
+    useEffect(() => {
+        setLoading(true)
+        getEmployees()
+    }, [getEmployees]);
+
     useEffect(() => {
         getLicenses()
-        setLoading(false)
-    }, [isLoading]);
+    }, [getLicenses]);
 
-    
     return (
         <>
 
@@ -100,20 +98,20 @@ const CargaDeLicencias = (props: CargaDeLicenciasProps,) => {
                     <Button>Volver atras</Button>
                 </Link>
             </div>
-            
-             
-            
+
+
+
 
             <Typography variant='h5' className={'mb-10'}></Typography>
             <LoadingIndicator show={isLoading} className={`flex flex-col items-start  transition-all duration-200`} >
-                
-            <div className="self-end mr-10 border-2 text-center  rounded-xl shadow-lg text-slate-800 hover:bg-gray-200 hover:text-teal-600 transition-all duration-300 cursor-pointer" onClick={handleAddOpen}>
-                <div className="m-4"> Crear Licencia</div>        
-            </div>
 
-            <AddResourceLicenceModal onSubmit={handleSubmit} onClose={handleClose} show={showAddModal} clients={[]} products={[]}/>
-            <AddResourceLicenceModal onSubmit={handleSubmit} onClose={handleClose} show={showEditModal} clients={[]} products={[]}/> 
-                
+                <div className="self-end mr-10 border-2 text-center  rounded-xl shadow-lg text-slate-800 hover:bg-gray-200 hover:text-teal-600 transition-all duration-300 cursor-pointer" onClick={handleAddOpen}>
+                    <div className="m-4"> Crear Licencia</div>
+                </div>
+
+                <AddResourceLicenceModal onSubmit={handleSubmit} onClose={handleClose} show={showAddModal} clients={[]} products={[]} />
+                <AddResourceLicenceModal onSubmit={handleSubmit} onClose={handleClose} show={showEditModal} clients={[]} products={[]} />
+
                 <TableContainer component={Paper} className="mt-10"  >
                     <Table>
                         <TableHead>
@@ -128,34 +126,34 @@ const CargaDeLicencias = (props: CargaDeLicenciasProps,) => {
                         </TableHead>
                         <TableBody>
                             {licenses
-                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row:License)=> <LicenseResourceTableRow row={row} key={row._id}/>)
-                            
-                            } 
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row: License) => <LicenseResourceTableRow row={row} key={row._id} />)
+
+                            }
                         </TableBody>
                         <TableFooter>
-                          <TableRow>
-                              <TablePagination
-                                  rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                                  colSpan={8}
-                                  count={4}
-                                  rowsPerPage={rowsPerPage}
-                                  page={page}
-                                  SelectProps={{
-                                      inputProps: {
-                                          'aria-label': 'rows per page',
-                                      },
-                                      native: true,
-                                  }}
-                                  onPageChange={handleChangePage}
-                                  onRowsPerPageChange={handleChangeRowsPerPage}
-                              />
-                          </TableRow>
-                      </TableFooter>
+                            <TableRow>
+                                <TablePagination
+                                    rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                                    colSpan={8}
+                                    count={4}
+                                    rowsPerPage={rowsPerPage}
+                                    page={page}
+                                    SelectProps={{
+                                        inputProps: {
+                                            'aria-label': 'rows per page',
+                                        },
+                                        native: true,
+                                    }}
+                                    onPageChange={handleChangePage}
+                                    onRowsPerPageChange={handleChangeRowsPerPage}
+                                />
+                            </TableRow>
+                        </TableFooter>
                     </Table>
                 </TableContainer>
             </LoadingIndicator>
-            
+
 
 
 
@@ -164,4 +162,4 @@ const CargaDeLicencias = (props: CargaDeLicenciasProps,) => {
     )
 }
 
-export default  CargaDeLicencias 
+export default CargaDeLicencias 
