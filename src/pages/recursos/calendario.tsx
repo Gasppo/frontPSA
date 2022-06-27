155fc592473a3160ea173ff7f1f93226b6bda144
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-06-27 12:48:18 -0300

    Calendario con select de empleados

--- src/pages/recursos/calendario.tsx
+++ src/pages/recursos/calendario.tsx
@@ -1,8 +1,10 @@
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import { useCallback, useEffect, useState } from 'react'
-import { Hours, Event, License } from '../../components/types/resourcesTypes'
+import { Hours, Event, License, SelectResource } from '../../components/types/resourcesTypes'
+import LoadingIndicator from '../../components/Loading/LoadingIndicator';
 import { isTypeNode } from 'typescript'
+import Select from 'react-select';
 import { Link } from 'react-router-dom'
 import { Button } from '@mui/material'
 
@@ -13,23 +15,46 @@ interface CalendarioProps {
 
 const Calendario = (props: CalendarioProps) => {
 
+    const [selected, setSelected] = useState<any>(0);
+    const [eventos, setEventos] = useState<Event[]>([]);
+    const [recursos, setRecursos] = useState<SelectResource[]>([]);
 
-    const [eventos, setEventos] = useState<Event[]>([])
+    const fetchEmployees = () => {
+        fetch('https://modulo-recursos-psa.herokuapp.com/employees')
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                let resources: SelectResource[] = [];
+                res.forEach((item: any) => {
+                    let proj = {
+                        label: item.Apellido + "," + item.Nombre,
+                        value: item.legajo
+                    }
+                    if (true)
+                        resources.push(proj)
+                })
+                setRecursos(resources);
+                console.log("A Falso")
+            })
+            .catch(err => {
+                console.log(JSON.stringify(err))
+            })
+        
+    }
 
     const fetchProjects = () => {
         fetch('https://modulo-recursos-psa.herokuapp.com/hours')
         .then(res => res.json())
         .then(res => {
-
-            let horas_por_empleado = res.filter((item:Hours) => item.hourAssignee === 3 )
+            let horas_por_empleado = res.filter((item:Hours) => item.hourAssignee === selected.value )
             let eventos2:Event[] = []
 
             horas_por_empleado.forEach((item:Hours) => {
                 let evento:Event = {
                     title: item.task.name + " (" + item.duration + "hs)",
-                    start: Date.parse(item.startingDate) + 60*60*24*1000,
+                    start: Date.parse(item.startingDate) + 60 * 60 * 24 * 1000,
                     timeZone: 'local',
-                    end: Date.parse(item.startingDate), //+ 60 * 60 * 1000 * item.duration,
+                    end: Date.parse(item.startingDate),
                     color: "blue",
                     allDay: false
                 }
@@ -40,7 +65,7 @@ const Calendario = (props: CalendarioProps) => {
             .then(res => res.json())
             .then(res => {
 
-                let licencias_por_empleado = res.filter((item:License) => item.licensedPersonCode === 3 )
+                let licencias_por_empleado = res.filter((item:License) => item.licensedPersonCode === selected.value )
 
                 licencias_por_empleado.forEach((item:License) => {
                     let evento:Event = {
@@ -54,8 +79,8 @@ const Calendario = (props: CalendarioProps) => {
                     eventos2.push(evento)
                 })
 
-                console.log(eventos2)
-                setEventos(eventos2)
+                console.log(eventos2);
+                setEventos(eventos2);
 
             })
             .catch(err => {
@@ -66,11 +91,17 @@ const Calendario = (props: CalendarioProps) => {
         .catch(err => {
             console.log(JSON.stringify(err))
         })
+
     }
 
     useEffect(() => {
-        fetchProjects()
-    }, []);
+        if(selected != 0){
+            fetchProjects();
+        }
+        else{
+            fetchEmployees();
+        }
+    }, [selected]);
 
     return (
         <>
@@ -79,12 +110,13 @@ const Calendario = (props: CalendarioProps) => {
                 <Button>Volver atras</Button>
             </Link>
         </div>
-            <FullCalendar
-                plugins={[dayGridPlugin]}
-                initialView="dayGridMonth"
-                displayEventTime={false}
-                events={eventos}
-            />
+        <Select options={recursos} onChange={(value) => setSelected(value)} className={`z-20`} />
+        <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth"
+            displayEventTime={false}
+            events={eventos}
+        />
         </>
     )
 }
