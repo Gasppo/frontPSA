2830b01c5b699ae2bc3fb0732af9d62fd2382958
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-06-29 23:37:48 -0300

    Loading en el calendario

--- src/pages/recursos/calendario.tsx
+++ src/pages/recursos/calendario.tsx
@@ -19,6 +19,7 @@ const Calendario = (props: CalendarioProps) => {
     const [selected, setSelected] = useState<any>(0);
     const [eventos, setEventos] = useState<Event[]>([]);
     const [recursos, setRecursos] = useState<SelectResource[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
     const fetchEmployees = () => {
         fetch('https://modulo-recursos-psa.herokuapp.com/employees')
@@ -35,92 +36,96 @@ const Calendario = (props: CalendarioProps) => {
                         resources.push(proj)
                 })
                 setRecursos(resources);
-                console.log("A Falso")
+                setLoading(false);
             })
             .catch(err => {
                 console.log(JSON.stringify(err))
             })
-        
+            
+
     }
 
     const fetchProjects = () => {
         fetch('https://modulo-recursos-psa.herokuapp.com/hours')
-        .then(res => res.json())
-        .then(res => {
-            let horas_por_empleado = res.filter((item:Hours) => item.hourAssignee === selected.value )
-            let eventos2:Event[] = []
-
-            horas_por_empleado.forEach((item:Hours) => {
-                let evento:Event = {
-                    title: item.task.name + " (" + item.duration + "hs)",
-                    start: Date.parse(item.startingDate) + 60 * 60 * 24 * 1000,
-                    timeZone: 'local',
-                    end: Date.parse(item.startingDate),
-                    color: "blue",
-                    allDay: false
-                }
-                eventos2.push(evento)
-            })
-
-            fetch('https://modulo-recursos-psa.herokuapp.com/licenses')
             .then(res => res.json())
             .then(res => {
+                let horas_por_empleado = res.filter((item: Hours) => item.hourAssignee === selected.value)
+                let eventos2: Event[] = []
 
-                let licencias_por_empleado = res.filter((item:License) => item.licensedPersonCode === selected.value )
-
-                licencias_por_empleado.forEach((item:License) => {
-                    let evento:Event = {
-                        title: item.licenseType,
-                        start: Date.parse(item.startingDate),
+                horas_por_empleado.forEach((item: Hours) => {
+                    let evento: Event = {
+                        title: item.task.name + " (" + item.duration + "hs)",
+                        start: Date.parse(item.startingDate) + 60 * 60 * 24 * 1000,
                         timeZone: 'local',
-                        end: Date.parse(item.startingDate) + 60 * 60 * 24 * 1000 * item.durationDays,
-                        color: "red",
-                        allDay: true
+                        end: Date.parse(item.startingDate),
+                        color: "blue",
+                        allDay: false
                     }
                     eventos2.push(evento)
                 })
 
-                console.log(eventos2);
-                setEventos(eventos2);
+                fetch('https://modulo-recursos-psa.herokuapp.com/licenses')
+                    .then(res => res.json())
+                    .then(res => {
+
+                        let licencias_por_empleado = res.filter((item: License) => item.licensedPersonCode === selected.value)
+
+                        licencias_por_empleado.forEach((item: License) => {
+                            let evento: Event = {
+                                title: item.licenseType,
+                                start: Date.parse(item.startingDate),
+                                timeZone: 'local',
+                                end: Date.parse(item.startingDate) + 60 * 60 * 24 * 1000 * item.durationDays,
+                                color: "red",
+                                allDay: true
+                            }
+                            eventos2.push(evento)
+                        })
+
+                        console.log(eventos2);
+                        setEventos(eventos2);
+                        setLoading(false);
+                    })
+                    .catch(err => {
+                        console.log(JSON.stringify(err))
+                    })
 
             })
             .catch(err => {
                 console.log(JSON.stringify(err))
             })
 
-        })
-        .catch(err => {
-            console.log(JSON.stringify(err))
-        })
-
     }
 
     useEffect(() => {
-        if(selected != 0){
+        setLoading(true);
+        if (selected != 0) {
             fetchProjects();
         }
-        else{
+        else {
             fetchEmployees();
         }
     }, [selected]);
 
     return (
         <>
-            <PageTitle label='Calendario'>
-            <div className="flex flex-row" >
-                <Link to={'/recursos/'} >
-                    <Button>Volver al inicio</Button>
-                </Link>
-            </div>
-            </PageTitle>
+            <LoadingIndicator show={loading} className="w-[95%] mb-10">
+                <PageTitle label='Calendario'>
+                    <div className="flex flex-row" >
+                        <Link to={'/recursos/'} >
+                            <Button>Volver al inicio</Button>
+                        </Link>
+                    </div>
+                </PageTitle>
 
-        <Select options={recursos} onChange={(value) => setSelected(value)} className={`z-20`} />
-        <FullCalendar
-            plugins={[dayGridPlugin]}
-            initialView="dayGridMonth"
-            displayEventTime={false}
-            events={eventos}
-        />
+                <Select options={recursos} onChange={(value) => setSelected(value)} className={`z-20`} />
+                <FullCalendar
+                    plugins={[dayGridPlugin]}
+                    initialView="dayGridMonth"
+                    displayEventTime={false}
+                    events={eventos}
+                />
+            </LoadingIndicator>
         </>
     )
 }
